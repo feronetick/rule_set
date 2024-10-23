bd91875391a8e6064446b35778bc7e3ff13f44d3
Максим Довгаль <maxdo@yandex-team.ru>
2024-10-24 03:24:47 +0500

    asdas

--- src/targets/sing_box.js
+++ src/targets/sing_box.js
@@ -9,6 +9,18 @@ export default class SingBoxTarget extends Target {
     }
 
     async save(ruleSet) {
+        const parsed = {};
+
+        for (const rule of ruleSet) {
+            for (const [field, value] of Object.entries(rule)) {
+                if (value) {
+                    parsed[field] = (parsed[field] || 0) + 1;
+                }
+            }
+        }
+
+        console.log(`Saving ${this.path}`, parsed);
+
         const content = JSON.stringify(new SingBoxRuleSet(ruleSet), null, 2);
         await fs.writeFile(this.path, content);
     }
